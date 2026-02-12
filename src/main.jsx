16392fa5ebab5e2855259e29a3480a70a5832123
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2026-02-13 00:41:23 +0530

    Update main entry configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,19 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { CartProvider } from './components/CartContext'
 import { ToastProvider } from './components/ToastContext'
-import { AuthProvider } from './context/AuthContext'
+
 import { WishlistProvider } from './context/WishlistContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <AuthProvider>
-        <WishlistProvider>
-          <CartProvider>
-            <ToastProvider>
-              <App />
-            </ToastProvider>
-          </CartProvider>
-        </WishlistProvider>
-      </AuthProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </CartProvider>
+      </WishlistProvider>
     </Router>
   </StrictMode>,
 )
