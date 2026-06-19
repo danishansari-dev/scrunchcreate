2d2c6a9250aa98d0d67d3f69b53351220a87f0e2
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2026-06-19 23:06:09 +0530

    refactor: add error boundary wrapper at root level in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { CartProvider } from './components/CartContext'
+import { CartProvider } from './context/CartContext'
 import { ToastProvider } from './components/ToastContext'
 import { WishlistProvider } from './context/WishlistContext'
 
