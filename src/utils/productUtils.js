1006cc249917ef91318eb4d8d4915b93114b7cbf
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2026-06-20 10:23:02 +0530

    chore: clean up old productUtils.js

--- src/utils/productUtils.js
+++
@@ -1,7 +0,0 @@
-// Helper function to create slug from title
-export function createSlug(title) {
-  return title
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/(^-|-$)/g, '')
-}
